24d093cacfdda72fd76ab34641e703d77f07f6f1
Pedroxian <44950875+pedroxian@users.noreply.github.com>
2018-11-21 02:33:52 +0100

    extensions for animate

---
+++ ISA-patch.bat
@@ -0,0 +1,5 @@
+set absurge..command delta:mouth /esp
+{eagle.eye /destination .unknown let.sp}
+<
+section.rule /line.12 _avemar ;leap.durex
+>
---
+++ NAT-klient.bat
@@ -0,0 +1,22 @@
+:local.terra /.epsylon.delta.blackhawk .drung -line43
+{
+338:glock.18 /ghost.recon -future.soldier /NET2.0
+    63.line _date.time -analystic /klon 6.0
+    universal.soldier rules/area.district
+    metro.2033 /beta.run
+    methanol.u -TANK.uvb a.c2 /E
+}
+        Solution:Epsylon -drive.mk2 /maschine
+        most.surrender :matrix :EU .tasked
+        klonbark.uvb /bllackhawk.rule /A
+{
+    A/rule .line.8:solution //naps.exploid.sony
+    sony.tank.u /TDK.casette TASCAM:J2K:client
+    migrate:HUB owl.task:visual.b
+    borland.id :migrate/debian _fusion.laps/leave
+    void:line.82 /B28.alfa 2.beta
+}
+set.improve /rule.B _self ;true.A ;false.rule
+    comport.2:FAX:task _run.paste /TEL:8377213454
+        <ignore.laps /leave ;true.nopes _diference.loud>
+wad.beta /.ogg -TASCAM .live/rec -loop.record:false;alfa
